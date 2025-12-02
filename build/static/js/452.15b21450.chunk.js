15195a3b11c9cfb988f81550e69c04c3040f5078
Harnoor Singh <harnoorsingh@Harnoors-MacBook-Pro.local>
2025-12-02 18:07:12 -0500

    Configure GitHub Pages deployment

--- build/static/js/452.15b21450.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkchat_app=self.webpackChunkchat_app||[]).push([[452],{452:(e,a,p)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=452.15b21450.chunk.js.map
---
+++ build/static/js/488.13e19d0d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchat_app=self.webpackChunkchat_app||[]).push([[488],{488:(e,a,p)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.13e19d0d.chunk.js.map
